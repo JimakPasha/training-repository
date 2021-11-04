0b7cabd212d2500fb2a1b59b3f01b0778721ee61
Pavel Hladki <->
2021-11-04 15:28:47 +0300

    add: singleton.js

---
+++ singleton.js
@@ -0,0 +1,26 @@
+class Universe {
+  constructor() {
+    if (Universe.exists) {
+      return Universe.instance;
+    }
+    Universe.instance = this;
+    Universe.exists = true;
+    this.size = 100;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  setSize(size) {
+    return (this.size = size);
+  }
+}
+
+let a = new Universe();
+let b = new Universe();
+
+console.log(a === b);
+console.log(a.getSize(), b.getSize());
+a.setSize(200);
+console.log(a.getSize(), b.getSize());
