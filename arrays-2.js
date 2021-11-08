0f0d110589cf40214f638e4ae29c8541e77f5e97
Pavel Hladki <->
2021-11-08 18:56:17 +0300

    add: all tasks

---
+++ arrays-2.js
@@ -0,0 +1,38 @@
+// 1. Вывести в консоль 'its valid', если во всех объектах поле a > b, вывести 'its invalid' если хотя бы в одном объекте a<=b
+
+const arr = [
+  {a: 2, b: 1},
+  {a: 5, b: 12},
+  {a: 95, b: 7}
+]
+
+const validationArr = (arr) => {
+  let checkValid = 'its valid';
+  arr.map((item) => item.a <= item.b ? ok = 'its invalid' : null);
+  console.log(checkValid);
+}
+
+validationArr(arr);
+
+// 2. На основе массива arr, создать массив объектов у которых поле b возведено в квадрат
+
+const squaringItemB = (arr) => {
+  return arr.map(item => {
+    return {...item, b: item.b * item.b};
+  });
+}
+
+console.log(squaringItemB(arr));
+
+
+// 3. С помощью метода/методов взять все поля a и b и последовательно поместить их в массив
+// пример получившегося массива: [2,1,5,12,95,7]
+// можно делать копию
+
+const expandArrObjects = (arr) => {
+  let newArr = [];
+  arr.map(item => newArr.push(item.a, item.b));
+  return newArr;
+}
+
+console.log(expandArrObjects(arr));
