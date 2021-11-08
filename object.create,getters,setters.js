6812243408574582fb199fb10cf1605159965234
Pavel Hladki <->
2021-11-08 15:51:19 +0300

    add conspect obj

--- object.create,getters,setters.js
+++ object.create,getters,setters.js
@@ -2,7 +2,7 @@ const person = Object.create(
   {
     calcAge() {
       console.log(new Date().getFullYear() - this.birthYear);
-    }
+    },
   },
   {
     name: {
@@ -19,14 +19,14 @@ const person = Object.create(
     },
     age: {
       get() {
-        return new Date().getFullYear() - this.birthYear
+        return new Date().getFullYear() - this.birthYear;
       },
       set(value) {
         document.body.style.background = 'red';
-      }
-    }
+      },
+    },
   }
-)
+);
 
 person.name = 'Max';
 
@@ -41,3 +41,43 @@ for (let key in person) {
 }
 
 // hasOwnProperty - не пробегается про прототипу
+
+// ------------------------------------------------
+
+// Object.getOwnPropertyDescriptor(obj, propertyName); - получаем полную инфу о свойстве
+
+const user = {
+  name: 'Jo',
+  surName: 'Smith',
+  age: 32,
+};
+
+let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+console.log(descriptor);
+
+// ------------------------------------------------
+
+// Object.defineProperty определяем новые или изменяем существующие свойства
+// Object.defineProperties несоклько свойств
+// Object.defineProperties(obj, {
+//   prop1: descriptor1,
+//   prop2: descriptor2
+//   // ...
+// });
+
+const man = {
+  name: 'Leo',
+  age: 30,
+};
+
+Object.defineProperty(man, 'name', {
+  writable: false,
+});
+
+man.name = 'Pete';
+
+console.log(man.name); // будет всё равно Leo, т.к. мы writable переназначили в false
+
+// ------------------------------------------------
+
+Object.freeze(obj); // Запрещает добавлять/удалять/изменять свойства. устанавливает configurable: false, writable: false для всех существующих свойств
---
+++ test.js
@@ -0,0 +1,27 @@
+//----------------------------------
+
+const user = {
+  name: 'John',
+  surName: 'Smith',
+  age: 30,
+  id: 'aSD3e344535Ktg354Pf'
+}
+//----------------------------------
+
+
+console.log(`afterCHANGE${user.id}`);
+
+
+
+//----------------------------------
+user.id = '000';
+//----------------------------------
+
+console.log(`beforeCHANGE${user.id}`);
+
+
+
+
+
+
+
