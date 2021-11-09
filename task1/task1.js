d6e83cb131814d329d42007c68cf58a734d88ebf
Pavel Hladki <->
2021-11-09 18:56:59 +0300

    add: task1

---
+++ task1/task1.js
@@ -0,0 +1,9 @@
+// 1. Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд и который несёт значение 100. delay(1000).then(value => console.log(‘Done with ’ + value)); // Done with 100
+
+const delay = (ms) => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(100);
+  }, ms)
+});
+
+delay(1000).then(value => console.log(`Done with ${value}`));
