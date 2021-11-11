882601c407f31cfc12b4d8b0fd9abb590883ab2b
Pavel Hladki <->
2021-11-11 12:18:45 +0300

    add: install wp

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+	const config = {
+		splitChunks: {
+			chunks: 'all'
+		}
+	}
+  if (isProd) {
+		config.minimizer = [
+			new CssMinimizerPlugin(),
+			new TerserWebpackPlugin()
+		]
+	}
+	return config;
+};
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+		main: ['@babel/polyfill', './index.js'],
+	},
+  output: {
+		filename: filename('js'),
+		path: path.resolve(__dirname, 'dist')
+	},
+  optimization: optimization(),
+  plugins: [
+    new HtmlWebpackPlugin({
+			template: './index.html',
+      minify: {
+				collapseWhitespace: isProd
+			}
+		}),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new MiniCssExtractPlugin({
+			filename: filename('css'),
+		}),
+    new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src/assets'),
+					to: path.resolve(__dirname, 'dist/assets'),
+					noErrorOnMissing: true,
+				},
+			],
+		}),
+	],
+  devServer: {
+		port: 8080,
+    hot: isDev
+	},
+  devtool: isDev ? 'source-map' : false,
+  module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+				test: /\.(png|jpg|svg|gif)$/,
+				use: ['file-loader']
+			},
+      {
+				test: /\.(ttd|woff|woff2|eot)$/,
+				use: ['file-loader']
+			},
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+		]
+	}
+};
