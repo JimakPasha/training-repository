03ceab161f2e4624ab38efb68ab5edfcedeba912
Pavel Hladki <->
2021-11-16 12:51:01 +0300

    feat: add components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <h2 className="header__title">Numbers App</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Math/Math.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import EnterBlock from '../../components/EnterBlock/EnterBlock';
+
+const Math = () => {
+  return <section className="math">{EnterBlock('math')}</section>;
+};
+
+export default Math;
