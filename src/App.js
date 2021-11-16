fc4d572ddcd2f956003cfbf5e44b3ea9d04b1cc8
Pavel Hladki <->
2021-11-16 14:47:12 +0300

    feat: add routing and cssTransition

--- src/App.js
+++ src/App.js
@@ -1,27 +1,71 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  useLocation,
+} from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Date from './pages/Date/Date';
 import Math from './pages/Math/Math';
 import Year from './pages/Year/Year';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 import Footer from './components/Footer/Footer';
 
 import './styles/global.scss';
+import './styles/cssTransition.scss';
 
 const App = () => {
   return (
-    <div className="app">
-      <Header />
-      <main className="main">
-        <div className="container">
-          <Home />
-          <Date />
-          <Math />
-          <Year />
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <main className="main">
+          <div className="container">
+            <PagesAll />
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+const PagesAll = () => {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        timeout={300}
+        classNames="cssTransition"
+        key={location.key}
+      >
+        <Switch location={location}>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/date">
+            <Date />
+          </Route>
+          <Route exact path="/math">
+            <Math />
+          </Route>
+          <Route exact path="/year">
+            <Year />
+          </Route>
+          <Route exact path="/error">
+            <ErrorPage />
+          </Route>
+          <Redirect to="/error">
+            <ErrorPage />
+          </Redirect>
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
---
+++ src/pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ErrorPage.scss';
+
+const Date = () => {
+  return (
+    <section className="errorPage">
+      <h4 className="errorPage__title">Ooops... 404 error</h4>
+      <p className="errorPage__text">This page does not exist</p>
+    </section>
+  );
+};
+
+export default Date;
